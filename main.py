77177c93c8e374db65efe065ce6b9c00b9f9f954
Drohung <rhemliv@gmail.com>
2025-11-23 10:31:35 -0500

    Added Rainbow End Text

--- main.py
+++ main.py
@@ -16,11 +16,14 @@ def main():
     phase_tk.set(1)
     root.update_idletasks
     text_id = canvas.create_text(960, 540, text="", font=("Arial", 24), fill="white", anchor='c')
-    file = open('phase1.txt')
+    file = open('Phases/Phase_1.txt')
+    rainbow_txt = open('Phases/rainbow_text.txt')
     full_text_tk = tk.StringVar()
     full_text_tk.set(file.readlines())
     txt_index_tk = tk.IntVar()
     txt_index_tk.set(0)
+    rb_index_tk = tk.IntVar()
+    rb_index_tk.set(0)
 
     def type_text(event, index=0):
         delay = 100
@@ -65,10 +68,38 @@ def main():
             root.attributes('-fullscreen', False)
         if root.attributes('-fullscreen') == False:
             root.attributes('-fullscreen', True)
+
+    def rainbow_end_text(event, letter_index=0, index=0, color_index=0):
+        delay = 100
+        delay2 = 50
+        if os.path.exists('Phases/rainbow_text.txt'):
+            rainbow_txt = open('Phases/rainbow_text.txt')
+            rainbow_list = rainbow_txt.readlines()
+            rainbow_line = str(rainbow_list[0])
+            rainbow_text = rainbow_line.replace('(', '').replace(')', '').replace("'", '').strip("\\n")
+            color_list = ["red", "orange", "yellow", "lawn green", "green", "blue", "cyan", "indigo", "magenta2"]
+            if index < len(rainbow_text):
+                color = color_list[index % len(color_list)]
+                canvas.itemconfigure(text_id, text='')
+                r_text = rainbow_text[index]
+                text_id == canvas.create_text(490 + ((index + 1) *60), 340, text=r_text, fill=color, font=("DotumChe", 48), anchor="c", tags=(index))
+                canvas.after(delay, lambda: rainbow_end_text(event, letter_index, index+1, color_index+1))
+            if index >= len(rainbow_text):
+                if color_index >= len(color_list):
+                    color_index = 0
+                if (letter_index - 1) > len(rainbow_text):
+                    letter_index = 0
+                if canvas.itemcget(index, 'text') == ' ':
+                    letter_index += 1
+                color2 = color_list[color_index]
+                canvas.itemconfigure(letter_index + 1, fill=color2)
+                canvas.after(delay2, lambda: rainbow_end_text(event, letter_index+1, index, color_index+1))
+        
     
     root.bind('<Escape>', commit_fullscreen)
     root.bind('<a>', type_text)
     root.bind('<d>', phase_change)
+    root.bind('<b>', rainbow_end_text)
     
     root.mainloop()
 
